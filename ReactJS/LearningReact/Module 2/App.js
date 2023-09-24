d2493cee4ea3e846adc5e6bac001eb0079aeb0e4
Kushal Seth <kushalseth1408@gmail.com>
2023-09-24 18:40:29 +0530

    Module 2

---
+++ ReactJS/LearningReact/Module 2/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const h1 = React.createElement(
+  "h1",
+  { id: "heading" },
+  "hello world from react"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(h1);
